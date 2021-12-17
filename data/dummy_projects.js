a009b8632741c00cb68e9f47c2cf4b9065ed6d46
Cleber <clered22@bergen.org>
2021-12-16 23:15:24 -0500

    commit

---
+++ data/dummy_projects.js
@@ -0,0 +1,90 @@
+module.exports =  [
+	{
+		"project_id" : 1,
+		"project_name" : "Breakfast",
+		"project_location" : "Lower",
+		"project_type" : "Main",
+		"project_date_ymd" : "2021-07-02",
+		"project_date" : "July 02 (Mon)",
+		"project_time" : "9:45 AM",
+		"project_duration" : 25,
+		"project_interest" : 3
+	},
+	{
+		"project_id" : 2,
+		"project_name" : "Opening Ceremony",
+		"project_location" : "Field",
+		"project_type" : "Workshop",
+		"project_date_ymd" : "2021-08-14",
+		"project_date" : "August 14 (Saturday)",
+		"project_time" : "12:00 AM",
+		"project_duration" : 75,
+		"project_interest" : 59
+	},
+	{
+		"project_id" : 3,
+		"project_name" : "Eating",
+		"project_location" : "Upper",
+		"project_type" : "Main",
+		"project_date_ymd" : "2021-01-21",
+		"project_date" : "January 21 (Tue)",
+		"project_time" : "9:00 AM",
+		"project_duration" : 45,
+		"project_interest" : 9
+	},
+	{
+		"project_id" : 4,
+		"project_name" : "Quidditch",
+		"project_location" : "Field",
+		"project_type" : "Sport",
+		"project_date_ymd" : "2021-10-29",
+		"project_date" : "October 29 (Fri)",
+		"project_time" : "2:00 PM",
+		"project_duration" : 70,
+		"project_interest" : 29
+	},
+	{
+		"project_id" : 5,
+		"project_name" : "Debate",
+		"project_location" : "Room 141",
+		"project_type" : "Workshop",
+		"project_date_ymd" : "2021-11-11",
+		"project_date" : "November 11 (Thursday)",
+		"project_time" : "4:30 PM",
+		"project_duration" : 60,
+		"project_interest" : 25
+	},
+	{
+		"project_id" : 6,
+		"project_name" : "Gametime",
+		"project_location" : "Commons",
+		"project_type" : "Workshop",
+		"project_date_ymd" : "2021-12-21",
+		"project_date" : "December 21 (Tue)",
+		"project_time" : "1:30 PM",
+		"project_duration" : 45,
+		"project_interest" :36
+	},
+	{
+		"project_id" : 7,
+		"project_name" : "Talking",
+		"project_location" : "Room 141",
+		"project_type" : "Workshop",
+		"project_date_ymd" : "2021-08-19",
+		"project_date" : "August 19 (Thu)",
+		"project_time" : "12:30 PM",
+		"project_duration" : 75,
+		"project_interest" : 4
+	},
+	{
+		"project_id" : 8,
+		"project_name" : "Chess",
+		"project_location" : "Lower",
+		"project_type" : "Sport",
+		"project_date_ymd" : "2021-12-04",
+		"project_date" : "December 04 (Mon)",
+		"project_time" : "3:30 PM",
+		"project_duration" : 40,
+		"project_interest" : 15
+	},
+]
